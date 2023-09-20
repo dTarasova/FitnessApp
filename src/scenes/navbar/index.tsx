d09ec4826e4b11af7afaadb66d4576296e41670b
Daria <d.al.tarasova@gmail.com>
2023-09-20 12:06:40 +0200

    toggled menu added

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -75,8 +75,41 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
           </div>
         </div>
       </div>
-    </nav>
-  )
-}
 
-export default Navbar
+      {/*Mobile Menu Modal */}
+      {!isAboveMediaScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl"> 
+          {/* Close Icon */}
+          <div className="flex justify-end p-12">
+            <button
+              onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <XMarkIcon className="h-6 w-6 text-gray-400"/>
+            </button>
+          </div>
+
+          {/* {Menu Items} */}
+          <div className={"ml-[33%] flex flex-col gap-10 text-2xl"}>
+            <Link 
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}/>
+            <Link 
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}/>
+            <Link 
+              page="Our classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}/>
+            <Link 
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}/>
+        </div>
+
+      </div>
+      )}
+    </nav>
+  );
+};
+export default Navbar;
