864eda1957c7bc10638f08e4ab7e9cc4c69afdae
Daria <d.al.tarasova@gmail.com>
2023-09-21 07:42:41 +0200

    container animation initial

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -1,18 +1,36 @@
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const childVariant = {
+  hidden: {opacity: 0, scale:0.9},
+  visible: {opacity: 0, scale:1},
+};
 
 type Props = {
     key : string;
     icon: JSX.Element;
     title : string;
     description : string;
+    setSelectedPage : (value: SelectedPage) => void;
 }
-const Benefit = ({} : Props) => {
+const Benefit = ({key,  icon, title, description, setSelectedPage} : Props) => {
   return (
-    <div className="mt-5 rounded-md border-2 border-gray-100">
-        <div>
-            
+    <motion.div variants={childVariant} className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center" >
+        <div className="mb-5 flex justify-center">
+            <div className=" rounded-full border-2 border-gray-100 bg-primary-100">
+              {icon}
+            </div>
         </div>
-      
-    </div>
+      <h4 className="font-bold">{title}</h4>
+      <p>{description}</p>
+      <AnchorLink
+        className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+        onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+        href={`#${SelectedPage.ContactUs}`}>
+        <p>Learn more</p>
+      </AnchorLink>
+    </motion.div>
   )
 }
 
--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -35,6 +35,13 @@ const Benefits = ({setSelectedPage}: Props) => {
         },
     ];
 
+    const container = {
+        hidden: {},
+        visible: {
+          transition: { staggerChildren: 0.2 },
+        },
+      };
+
   return (
     <section
         id="benefits"
@@ -42,30 +49,44 @@ const Benefits = ({setSelectedPage}: Props) => {
         >
         <motion.div onViewportEnter={() => {setSelectedPage(SelectedPage.Benefits)}}>
             {/* {Header} */}
-            <div className="md:my-5 md:w-3/5">
+            <motion.div 
+                className="md:my-5 md:w-3/5"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 },
+            }}>
                 <HText>MORE THAN JUST A GYM</HText>
                 <p className="my-5 text-sm">
                 We provide world class fitness equipment, trainers and classes to
                 get you to your ultimate fitness goals with ease. We provide true
                 care into each and every member.
                 </p>
-            </div>
+            </motion.div>
 
             {/* {Benefits} */}
-            <div className="md:flex items-center justify-between gap-8 mt:5">
+            <motion.div 
+                className="mt-5 items-center justify-between gap-8 md:flex"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={container}
+            >
                 {benefits.map((benefit: BenefitType) => (
                     <Benefit 
                         key={benefit.title}
                         icon={benefit.icon}
                         title={benefit.title}
                         description={benefit.description}
+                        setSelectedPage={setSelectedPage}
                     />
                 ))}
 
-            </div>
+            </motion.div>
         </motion.div>
-
-      
     </section>
   )
 }
